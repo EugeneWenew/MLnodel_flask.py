d5a4d72dd9557f70bfffcb0f3628af6ae5d9c5b9
EugeneWenew <venevevg@gmail.com>
2021-09-26 19:47:49 +0700

    another one method

--- model2.py
+++ model2.py
@@ -4,37 +4,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_excel('d:\set.xlsx', header=0)
 df_1 = df.drop(['Персона'], axis=1)
 X = df_1
-
 X.head()
-from sklearn.preprocessing import StandardScaler
 StandardScaler().fit_transform(X)
 model = KMeans(n_clusters = 10)
-
 model.fit(X)
 X["cluster"] = model.fit_predict(X)
-
 plt.scatter(X['Стаж вождения, лет'], X['Убыточность, %'], c = X['cluster'])
 plt.scatter(X['Возраст, лет'], X['Убыточность, %'], c = X['cluster'])
 plt.scatter(X['Уровень заработной платы, руб/год'], X['Убыточность, %'], c = X['cluster'])
 wcss = []
-
 for i in range(1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
-
-
 plt.plot(range(1, 11), wcss)
 model = KMeans(n_clusters = 2)
-
 model.fit(X)
-
-X['cluster'] = model.fit_predict(X)
-
+#X['cluster'] = model.fit_predict(X)
 sns.boxplot (x = "cluster", y="Убыточность, %", data=X)
 sns.boxplot (x = "cluster", y="Возраст, лет", data=X)
 sns.boxplot (x = "cluster", y="Стаж вождения, лет", data=X)
@@ -51,12 +42,13 @@ def pred_values(input_data):
     df_1 = df.drop(['Персона'], axis=1)
     X = df_1
     StandardScaler().fit_transform(X)
-    kmeans = KMeans(init="k-means++", n_clusters=4, n_init=12)
-    kmeans.fit(X)
+    model = KMeans(init="k-means++", n_clusters=2)
+    model.fit(X)
+    #X['cluster'] = model.fit_predict(X)
     X_test = pd.DataFrame(input_data)
     X_test = X_test.transpose()
     scaled_test = StandardScaler().fit_transform(X_test)
-    predicted_label = kmeans.predict(scaled_test)
+    predicted_label = model.predict(scaled_test)
     return predicted_label
 
 df_test = [24, 6, 348, 139419]
